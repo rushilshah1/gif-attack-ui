de7f500d78715d3f561bd7c1f2f7679645df2fb5
Rushil Shah <rushilrshah1@gmail.com>
2020-05-25 19:26:54 -0400

    Add amplify

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { Game } from './game/Game';
 import { Home } from './home/Home';
 import { Router, BrowserRouter, Route, Redirect, useHistory } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
-import * as dotenv from "dotenv";
 
 
 const StyledHomeIcon = withStyles({
