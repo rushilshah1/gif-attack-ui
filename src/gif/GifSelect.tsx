3cd97e9e0ae7efdeaf2c2b66b312ccc5b6b83422
Rushil Shah <rushilrshah1@gmail.com>
2020-07-01 21:05:00 -0400

    Center Gif Select

--- src/gif/GifSelect.tsx
+++ src/gif/GifSelect.tsx
@@ -3,6 +3,7 @@ import './GifSelect.scss';
 import ENVIRONMENT from '../common/environments';
 import { ENVIRONMENT_LOCAL } from '../common/constants';
 import ReactGiphySearchbox from 'react-giphy-searchbox';
+import { Grid } from '@material-ui/core';
 
 interface GifSelectProps {
     selectGif: (gif: any, searchText: string) => void;
@@ -13,26 +14,28 @@ export const GifSelect: React.FC<GifSelectProps> = props => {
     const [gifSearchInput, setGifSearchInput] = useState<string>('');
 
     return (
-        <div className="gif-selection" >
-            <ReactGiphySearchbox
-                apiKey={ENVIRONMENT.GIPHY_KEY}
-                onSelect={(gif) => {
-                    props.selectGif(gif, gifSearchInput)
-                }}
-                onSearch={(text) => setGifSearchInput(text)}
-                wrapperClassName={"searchbox-wrapper"}
-                searchFormClassName={"searchbox-search"}
-                listWrapperClassName={"searchbox-list"}
-                listItemClassName={"searchbox-image"}
-                gifListHeight={350}
-                gifPerPage={10}
-                masonryConfig={
-                    [
-                        { columns: 2, imageWidth: 150, gutter: 10 },
-                        { mq: '700px', columns: 3, imageWidth: 190, gutter: 10 },
-                        // { mq: '1000px', columns: 4, imageWidth: 200, gutter: 10 },
-                    ]}
-            />
-        </div >
+        <Grid container direction="row" justify="center">
+            <div className="gif-selection" >
+                <ReactGiphySearchbox
+                    apiKey={ENVIRONMENT.GIPHY_KEY}
+                    onSelect={(gif) => {
+                        props.selectGif(gif, gifSearchInput)
+                    }}
+                    onSearch={(text) => setGifSearchInput(text)}
+                    wrapperClassName={"searchbox-wrapper"}
+                    searchFormClassName={"searchbox-search"}
+                    listWrapperClassName={"searchbox-list"}
+                    listItemClassName={"searchbox-image"}
+                    gifListHeight={350}
+                    gifPerPage={10}
+                    masonryConfig={
+                        [
+                            { columns: 2, imageWidth: 150, gutter: 10 },
+                            { mq: '700px', columns: 3, imageWidth: 190, gutter: 10 },
+                            // { mq: '1000px', columns: 4, imageWidth: 200, gutter: 10 },
+                        ]}
+                />
+            </div >
+        </Grid>
     )
 }
